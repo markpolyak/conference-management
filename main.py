935c4452b5f5bc7917e8c6b5a5084450db8c3b05
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-07-21 00:19:52 +0300

    update post and put requests

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
     
-    
 
 
 GOOGLE_SHEET_ID = os.getenv("TOKEN")
@@ -35,17 +34,34 @@ class Conference(BaseModel):
     url: str = None
     email: str
 
+
+# Модель данных для конференции
+class ConferenceUpdate(BaseModel):
+    id: int  = None
+    name_rus: str = None
+    name_rus_short: str = None
+    name_eng: str = None
+    name_eng_short: str = None
+    registration_start_date: str = None
+    registration_end_date: str = None
+    submission_start_date: str = None
+    submission_end_date: str = None
+    conf_start_date: str = None
+    conf_end_date: str = None
+    organized_by: str = None
+    url: str = None
+    email: str = None
+
 app = FastAPI()
 
-
+path = os.path.abspath(os.path.dirname(__file__)) + '\credentials.json'
 
 # Загрузка учетных данных и авторизация в Google Sheets API
-with open('E:/conference-management/credentials.json') as f:
+with open(path) as f:
     creds_data = json.load(f)
 
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_data, ['https://www.googleapis.com/auth/spreadsheets'])
 client = gspread.authorize(credentials)
-
 
 
 # Получение экземпляра таблицы
@@ -53,18 +69,20 @@ sheet = client.open_by_key(GOOGLE_SHEET_ID).sheet1
 
 # Функция для проверки валидности токена
 def is_valid_token(token: str) -> bool:
+    
+    
     token_sheet = os.getenv("TOKEN")
+    
     if token == token_sheet:
         return True
-    
     else:
         return False
+
 
 # Обработчик корневого пути
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 
 # Получение списка конференций
@@ -104,13 +122,21 @@ def get_conferences(filter: str = Query("active", enum=["all", "active", "past",
 
     return result
 
+
 # Эндпоинт для получения информации о конференции по ее ID
-@app.get('/conferences/{id}')
-def get_conference(id: int, authorization: str = Header(None)):
-    
-    try:    
-        row = sheet.row_values(id)
-        conference = Conference(**dict(zip(Conference.__fields__.keys(), row)))
+@app.get('/conferences/{conference_id}')
+def get_conference(conference_id: int, authorization: str = Header(None)):
+    try:
+        # Чтение данных из таблицы Google Sheets
+        data = sheet.get_all_records()
+
+        # Поиск конференции по значению поля "id"
+        conference = next((record for record in data if record["id"] == conference_id), None)
+
+        if conference is None:
+            raise HTTPException(status_code=404, detail='Конференция не найдена')
+
+        conference = Conference(**conference)
 
         # Проверка валидности токена авторизации
         if authorization and is_valid_token(authorization.replace("Bearer ", "")):
@@ -132,12 +158,11 @@ def create_conference(conference: Conference, authorization: str = Header(None))
         raise HTTPException(status_code=403, detail='Неверный токен авторизации')
 
     # Проверка наличия идентификатора конференции
-    if conference.id is not None:
+    if conference.id is None:
         raise HTTPException(status_code=400, detail='Идентификатор конференции не должен указываться при создании новой записи')
 
     # Добавление данных в таблицу Google Sheets
     row_data = conference.dict()
-    row_data.pop("id")  # Удаляем поле "id" из данных, чтобы не добавить его в таблицу
     sheet.insert_row(list(row_data.values()), index=len(sheet.get_all_records()) + 2)  # +1 для заголовков и +1 для новой записи
 
     # Получение порядкового номера добавленной в таблицу строки (id)
@@ -151,67 +176,43 @@ def create_conference(conference: Conference, authorization: str = Header(None))
 
 
 
-
 # Редактирование конференции по идентификатору
 @app.put('/conferences/{conference_id}')
-def update_conference(conference_id: int, conference_data: Conference, authorization: str = Header(None)):
+def update_conference(conference_id: int, conference_data: ConferenceUpdate, authorization: str = Header(None)):
     # Проверка наличия валидного токена авторизации
     if not authorization or not is_valid_token(authorization.replace("Bearer ", "")):
         raise HTTPException(status_code=403, detail='Неверный токен авторизации')
 
     # Получение текущих данных о конференции из Google Sheets
     try:
-        row = sheet.row_values(conference_id)
-    except IndexError:
+        # Используйте метод `find` вместо `row_values` для поиска записи по id
+        record = sheet.find(str(conference_id))
+    except gspread.exceptions.CellNotFound:
         raise HTTPException(status_code=404, detail='Конференция с указанным id не найдена')
 
+    row = sheet.row_values(record.row)
+    
     # Создание объекта Conference с текущими данными о конференции
-    current_conference = Conference(**dict(zip(Conference.__fields__.keys(), row)))
+    current_conference = ConferenceUpdate(**dict(zip(ConferenceUpdate.__fields__.keys(), row)))
 
     # Обновление полей в текущем объекте Conference значениями из запроса, если они указаны
     for field, value in conference_data.dict(exclude_unset=True).items():
         setattr(current_conference, field, value)
 
+    array  = list(current_conference.dict().values())
+    array[0] = str(array[0])
+
+    new_array = []
+    new_array.append(array)
+ 
     # Обновление данных в таблице Google Sheets
-    sheet.update(conference_id, current_conference.dict().values())
+    sheet.update('A'+str(record.row)+':N'+str(record.row), new_array)
 
     # Возвращаем JSON с обновленной информацией о конференции
     response_data = current_conference.dict()
     response_data["google_spreadsheet"] = GOOGLE_SHEET_ID
     response_data["id"] = conference_id
     return response_data
-
-
-
-
-# Добавление кода для вывода содержимого всей таблицы
-@app.get('/all_conferences')
-def get_all_conferences():
-    # Чтение данных из таблицы Google Sheets
-    data = sheet.get_all_records()
-    
-    # Оставляем только необходимые поля
-    result = [
-        {
-            "id": record["id"],
-            "name_rus_short": record["name_rus_short"],
-            "name_eng_short": record["name_eng_short"],
-            "conf_start_date": record["conf_start_date"],
-            "conf_end_date": record["conf_end_date"]
-        }
-        for record in data
-    ]
-
-    # Выводим содержимое таблицы в консоль
-    for conference in result:
-        print(f"ID: {conference['id']}")
-        print(f"Название (рус.): {conference['name_rus_short']}")
-        print(f"Название (англ.): {conference['name_eng_short']}")
-        print(f"Дата начала конференции: {conference['conf_start_date']}")
-        print(f"Дата окончания конференции: {conference['conf_end_date']}")
-        print("")
-
-    return result
 
 
 if __name__ == "__main__":
